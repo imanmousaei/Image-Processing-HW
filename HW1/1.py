9542dd0b3bd1bd7b21581c8b43ffb3ae292052c7
ImanMousaei <imanmousaei1379@gmail.com>
2023-04-13 13:04:45 +0330

    Find right parameters

--- HW1/1.py
+++ HW1/1.py
@@ -8,7 +8,7 @@ def detect_faces(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Detect faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.2, 5, minSize=[100, 100])
     return faces
 
 
@@ -16,6 +16,15 @@ def draw_rectangle(img, contours):
     # Draw rectangle around the faces
     for (x, y, w, h) in contours:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    return img
+
+def resize_image(img, scale_percent):
+    height = int(img.shape[0] * scale_percent / 100)
+    width = int(img.shape[1] * scale_percent / 100)
+    dim = (width, height)
+    
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    return resized
 
 
 def show_image(img):
@@ -28,5 +37,6 @@ if __name__ == '__main__':
     img = cv2.imread(image_path)
 
     faces = detect_faces(img)
-    draw_rectangle(img, faces)
+    img = draw_rectangle(img, faces)
+    img = resize_image(img, 20)
     show_image(img)
