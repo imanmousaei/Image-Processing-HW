00597979f70ade2bfa84f3779c757e2577a06401
ImanMousaei <imanmousaei1379@gmail.com>
2023-04-14 18:57:24 +0330

    Use opencv to read and write

--- HW1/2.py
+++ HW1/2.py
@@ -1,15 +1,13 @@
 import numpy as np
-from PIL import Image
+import cv2
 from sklearn.cluster import KMeans
 
 
 def index_compress(image_path, num_colors=8):
-    # img = cv2.imread(image_path)
-    img = Image.open(image_path)
-    img_array = np.array(img)
+    img = cv2.imread(image_path)
 
     # Reshape each channel to a 1D array of pixels
-    pixels = img_array.reshape(-1, 3)
+    pixels = img.reshape(-1, 3)
 
     # Use KMeans to find most common colors
     kmeans = KMeans(n_clusters=num_colors, random_state=0, n_init='auto').fit(pixels)
@@ -19,14 +17,14 @@ def index_compress(image_path, num_colors=8):
 
     # Replace each pixel with the closest color
     new_pixels = np.array([kmeans.cluster_centers_[label] for label in kmeans.labels_])
-    new_img_array = new_pixels.reshape(img_array.shape)
+    new_img = new_pixels.reshape(img.shape)
     
-    # print(new_img_array)
-    
-    new_img = Image.fromarray(new_img_array.astype('uint8'), 'RGB')
-    new_img.save('output/2-compressed_image1.jpg')
+    # print(new_img)
+
+    return new_img
 
 
 if __name__ == '__main__':
-    image_path = 'img/image1.jpg'
-    index_compress(image_path)
+    image_path = 'img/image2.jpg'
+    compressed_img = index_compress(image_path)
+    cv2.imwrite(f'output/2-compressed_image2.jpg', compressed_img)
