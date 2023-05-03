ca2e3d3cc20e553f44a3444e7b9c3f3892650277
ImanMousaei <imanmousaei1379@gmail.com>
2023-05-04 00:17:28 +0330

    Add PSNR loss

--- HW2/2.py
+++ HW2/2.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def resize_image(img, scale_percent):
@@ -22,6 +23,13 @@ def MSE(img1, img2):
     mse_loss = np.mean(squared_diff)
     return mse_loss
 
+def PSNR(img1, img2):
+    mse = MSE(img1, img2)
+    l = 255
+    psnr = 10 * np.log10(l*l/mse)
+    return psnr
+    
+    
 def gaussian_filter(img, kernel_size):
     # apply Gaussian blur with kernel size of kernel_size x kernel_size and standard deviation of 0
     blur = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
@@ -34,12 +42,26 @@ filenames = [
     'baboon.bmp'
 ]
 
+loss_function = 'psnr'
+
 if __name__ == '__main__':
     for imagename in filenames:
-        img = cv2.imread(f'img/{imagename}')
-        
-        kernel_size = 5
-        blur = gaussian_filter(img, kernel_size)
-        
-        loss = MSE(img, blur)
-        print(f'loss of {imagename} with kernel_size = {kernel_size} is {loss}')
+        losses = []
+        for kernel_size in range(1,102,2):
+            img = cv2.imread(f'img/{imagename}')
+            
+            blur = gaussian_filter(img, kernel_size)
+            
+            if loss_function.lower() == 'mse':
+                loss = MSE(img, blur)
+            else:
+                loss = PSNR(img, blur)
+            
+            losses.append(loss)
+            print(f'loss of {imagename} with kernel_size = {kernel_size} is {loss}')
+
+        plt.plot(losses)
+        plt.title(imagename)
+        plt.xlabel('MSE')
+        plt.ylabel('Kernel Size')
+        plt.show()
